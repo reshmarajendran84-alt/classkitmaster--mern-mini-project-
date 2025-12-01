f57ed074ae20ab5ae5ac12bd0e9e02abb8e8d03d
Reshma Rajendran <reshmarajendran84@gmail.com>
2025-12-01 10:32:58 +0530

    Fix Render port listening

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ app.get("/",(req,res)=>{
   console.log("server is runing");
 });
 app.use("/api/products", productRoutes);
-
-app.listen(process.env.PORT || 5000, () =>
-  console.log("Server running on port", process.env.PORT || 5000)
-);
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+  console.log("Server running");
+});
